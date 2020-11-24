3c1340169b8a82e6b75390ca8b0bf6854227d1b9
Julien Danjou <julien@danjou.info>
2020-11-24 16:19:46 +0100

    chore(mypy): tweak mypy options and add more typing (#63)
    - Enable `ignore_missing_imports` so we don't fail on missing typing for
      external libs
    - Disallow incomplete defs to make sure we have fully documented type on every
      typed function
    - Warn on unused configs and ignore directives
    - Disallow usage of Any
    - Specify files to check
    
    Co-authored-by: Kyle Verhoog <kyle@verhoog.ca>

--- riotfile.py
+++ riotfile.py
@@ -40,7 +40,7 @@ venv = Venv(
         ),
         Venv(
             name="typing",
-            command="mypy riot/ tests/",
+            command="mypy",
             pkgs={
                 "mypy": [""],
                 "pytest": [""],
