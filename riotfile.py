6a7fda38894428a0f3c6398179838b4c9f35e725
Kyle Verhoog <kyle@verhoog.ca>
2020-11-24 14:35:32 -0500

    Add support for singleton values (#62)
    This allows pkgs, env and pys to have singleton values which makes the
    configuration more ergonomic.

--- riotfile.py
+++ riotfile.py
@@ -1,49 +1,49 @@
 from riot import Venv
 
 venv = Venv(
-    pys=[3.8],
+    pys=3.8,
     venvs=[
         Venv(
             name="test",
             command="pytest --color=yes --cov=riot/ --cov=tests/ --cov-append --cov-report= {cmdargs} tests/",
             env={
-                "LC_ALL": ["C.UTF-8"],
-                "LANG": ["C.UTF-8"],
+                "LC_ALL": "C.UTF-8",
+                "LANG": "C.UTF-8",
             },
             pys=[3.7, 3.8, 3.9],
             pkgs={
-                "pytest": [""],
-                "pytest-cov": [""],
-                "mock": [""],
+                "pytest": "",
+                "pytest-cov": "",
+                "mock": "",
             },
         ),
         Venv(
             name="check_format",
             command="black --check .",
             pkgs={
-                "black": ["==20.8b1"],
+                "black": "==20.8b1",
             },
         ),
         Venv(
             name="flake8",
             command="flake8",
             pkgs={
-                "flake8": [""],
-                "flake8-blind-except": [""],
-                "flake8-builtins": [""],
-                "flake8-docstrings": [""],
-                "flake8-logging-format": [""],
-                "flake8-rst-docstrings": [""],
+                "flake8": "",
+                "flake8-blind-except": "",
+                "flake8-builtins": "",
+                "flake8-docstrings": "",
+                "flake8-logging-format": "",
+                "flake8-rst-docstrings": "",
                 # needed for some features from flake8-rst-docstrings
-                "pygments": [""],
+                "pygments": "",
             },
         ),
         Venv(
             name="typing",
             command="mypy",
             pkgs={
-                "mypy": [""],
-                "pytest": [""],
+                "mypy": "",
+                "pytest": "",
             },
         ),
         Venv(
@@ -51,15 +51,15 @@ venv = Venv(
             name="codecov",
             command="bash <(curl -s https://codecov.io/bash)",
             pkgs={
-                "coverage": [""],
+                "coverage": "",
             },
         ),
         Venv(
             name="docs",
             command="sphinx-build -W -b html docs docs/_build/",
             pkgs={
-                "sphinx": ["==3.3"],
-                "reno": [""],
+                "sphinx": "==3.3",
+                "reno": "",
             },
         ),
     ],
