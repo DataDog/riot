044290eff2e85c9cd3d7fb2030bc4f93a25ae7f5
brettlangdon <brett.langdon@datadoghq.com>
2021-05-17 17:46:48 -0400

    add unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,13 @@
+import sys
+
 import pytest
 from riot.riot import Interpreter
+
+
+@pytest.fixture
+def current_interpreter() -> Interpreter:
+    version = ".".join((str(sys.version_info[0]), str(sys.version_info[1])))
+    return Interpreter(version)
 
 
 @pytest.mark.parametrize(
@@ -26,3 +34,18 @@ def test_interpreter(v1, v2, equal):
         assert Interpreter(v1) != Interpreter(v2)
         assert hash(Interpreter(v1)) != hash(Interpreter(v2))
         assert repr(Interpreter(v1)) != repr(Interpreter(v2))
+
+
+def test_venv_path(current_interpreter: Interpreter):
+    py_version = "".join((str(_) for _ in sys.version_info[:3]))
+    assert current_interpreter.venv_path() == ".riot/venv_py{}".format(py_version)
+
+
+def test_sitepackages_path(current_interpreter: Interpreter):
+    py_full_version = "".join((str(_) for _ in sys.version_info[:3]))
+    py_dot_version = ".".join((str(_) for _ in sys.version_info[:2]))
+
+    expected = ".riot/venv_py{}/lib/python{}/site-packages".format(
+        py_full_version, py_dot_version
+    )
+    assert current_interpreter.site_packages_path().endswith(expected)
