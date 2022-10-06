27f9b7ad9dd4d2134a00865b89fec215b5d69dd5
Yun Kim <yun.kim@datadoghq.com>
2022-10-06 13:25:21 -0400

    Formatting

--- tests/data/diff_pys_riotfile.py
+++ tests/data/diff_pys_riotfile.py
@@ -7,7 +7,9 @@ venv = Venv(
             command="exit 0",
             pys=["3.5", "3.8", "3.6", "3.9", "3.7", "2.7"],
             venvs=[
-                Venv(pkgs={"pytest": ["==5.4.3", ""]},),
+                Venv(
+                    pkgs={"pytest": ["==5.4.3", ""]},
+                ),
             ],
         ),
     ]
