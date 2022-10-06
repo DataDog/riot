8a289ae21acf56be5dbaa5ba79b0f4aa0ad929d6
Yun Kim <yun.kim@datadoghq.com>
2022-10-06 13:30:09 -0400

    Add packaging dependency to tests, add comment in test riotfile

--- riotfile.py
+++ riotfile.py
@@ -13,11 +13,13 @@ venv = Venv(
                 "pytest-xdist": latest,
                 "mock": latest,
                 "typing-extensions": latest,
+                "packaging": latest,
             },
         ),
         Venv(
             pkgs={
                 "black": "==22.6.0",
+                "packaging": latest,
             },
             venvs=[
                 Venv(
@@ -43,6 +45,7 @@ venv = Venv(
                 "flake8-rst-docstrings": latest,
                 # needed for some features from flake8-rst-docstrings
                 "pygments": latest,
+                "packaging": latest,
             },
         ),
         Venv(
@@ -51,6 +54,7 @@ venv = Venv(
             pkgs={
                 "mypy": latest,
                 "pytest": latest,
+                "packaging": latest,
             },
             create=True,
         ),
--- tests/data/diff_pys_riotfile.py
+++ tests/data/diff_pys_riotfile.py
@@ -5,6 +5,7 @@ venv = Venv(
         Venv(
             name="test",
             command="exit 0",
+            # DEV: purposely out of order so we can verify sorting in tests
             pys=["3.5", "3.8", "3.6", "3.9", "3.7", "2.7"],
             venvs=[
                 Venv(
