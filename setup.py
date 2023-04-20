e667ea3c2ffe3f875b7a26f0a847c7fa013e9cac
Federico Mon <federico.mon@datadoghq.com>
2023-04-20 13:59:26 +0200

    chore(releasenotes): add release note

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "dataclasses; python_version<'3.7'",
         "click>=7",
-        "virtualenv==20.20.0",
+        "virtualenv<=20.20.0",
         "rich",
         "pexpect",
         "packaging",
